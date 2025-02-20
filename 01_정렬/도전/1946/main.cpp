1b8335577b421bb359cc1e615e82b58086b2aa2d
emilyminkyounglee <minkyoungemilylee@ewha.ac.kr>
2025-02-21 01:27:59 +0900

    [정렬 도전과제] 2월 21일
    ### 인적사항
    학번: 2466040
    이름: 이민경
    
    ### 과제제출
    기존제출: 1946
    푸는 중: 9375

--- 01_정렬/도전/1946/main.cpp
+++ 01_정렬/도전/1946/main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
-using namespace std;
-int main() {
+#include<vector>
+#include <algorithm>
 
+using namespace std;
+
+void appliedHumans(int n, vector<pair<int, int>>& arr) {
+    arr.clear();
+    for(int i = 0; i<n; i++) {
+        int cv, interview;
+        cin >> cv >> interview;
+        arr.emplace_back(cv, interview);
+    }
+}
+bool secondCompare(const pair<int, int>& a, const pair<int, int>& b) {
+    return a.second < b.second;
+}
+int maxAcceptiance(vector<pair<int, int>>& arr) {
+
+    int num = 0;
+    sort(arr.begin(), arr.end());
+    int maximum_1 = arr[0].second;
+    for(int i = 0; i <arr.size(); i++) {
+        if(arr[i].second <= maximum_1) {
+            num++;
+            maximum_1 = arr[i].second;
+        }
+    }
+
+
+    return num;
+}
+
+int main() {
+    int t, n;
+    vector<pair<int, int>> v;
+    cin >> t;
+    for(int i = 0; i<t; i++) {
+        cin >> n;
+        appliedHumans(n, v);
+        cout << maxAcceptiance(v) << "\n";
+    }
     return 0;
 }
